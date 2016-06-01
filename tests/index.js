de2701519b7baeafe895265fa1051a4c1a06593c
goenning <oenning.ti@gmail.com>
2016-06-01 19:29:08 -0300

    changing ports

--- tests/index.js
+++ tests/index.js
@@ -6,11 +6,8 @@ var http = require('http');
 var request = require('request');
 var ip = require('docker-ip');
 
-console.log(ip())
-
 var redis = require('redis');
-var client = redis.createClient(6379, ip());
-console.log(client)
+var client = redis.createClient(6060, ip());
 
 describe('Redis Tests', function() {
   var server = http.createServer((request, response) => {
